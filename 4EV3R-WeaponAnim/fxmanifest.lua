2cb08a5907c3b84f4311b4b26779d0c63a899b22
4EV3R <117762380+4EV3R@users.noreply.github.com>
2025-05-18 23:30:47 +0200

    Script Push

---
+++ 4EV3R-WeaponAnim/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '4EV3R Scripts'
+description 'Weapon Holding Animation Script Made for OX | Made by 4EV3R Scripts'
+version '1.0'
+
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
